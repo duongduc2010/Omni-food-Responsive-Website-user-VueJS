ba72cba6a112ae8213c787f3b5695e8ad5201f9b
Duong Tung Duc <tungduc@vnpt.vn>
2024-07-09 17:04:14 +0700

    first commit

---
+++ ResponseWebsiteOmnifood/src/views/headerView/headerView.vue
@@ -0,0 +1,22 @@
+<template src="./headerView.html"></template>
+<style src="./headerView.css"></style>
+<script>
+    export default {
+        name: "headerViews",
+        components: {
+            
+        },
+        data(){
+            return {
+
+            }
+        },
+        methods: {
+            
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ ResponseWebsiteOmnifood/src/views/home/home.vue
@@ -0,0 +1,27 @@
+<template src="./home.html"></template>
+<style src="./home.css">
+
+</style>
+<script>
+    export default {
+        name: "home",
+        components: {
+
+        },
+        data(){
+            return {
+
+            }
+        },
+        methods: {
+            link(){
+                // let query = {
+                //     name: "123"
+                // }
+                // let routeData = this.$router.resolve({"name": "listProducts",query: query})
+                // window.open(routeData.href)
+            }
+        },
+    }
+</script>
+
