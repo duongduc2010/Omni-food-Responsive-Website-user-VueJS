d89c7a68397f4f9f340d76bd0dc7dd669c47dd1d
Duong Tung Duc <tungduc@vnpt.vn>
2024-07-16 16:59:36 +0700

    update

--- ResponseWebsiteOmnifood/src/main.js
+++ ResponseWebsiteOmnifood/src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './style.css'
 import './general.css'
+import './queries.css'
 import router from './router'
 import Icon from './components/icon.vue'
 
